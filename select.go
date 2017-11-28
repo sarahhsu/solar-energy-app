0f4c3c1edb572e9cb6a191407ac9f6228eb7d927
Sarah Hsu <sarahhsu@Sarahs-MacBook-Air.local>
2017-11-28 01:04:08 -0500

    make it better

--- select.go
+++ select.go
@@ -1,30 +1,29 @@
 package main
 
 import (
-  "net/http"
-  "log"
-  "html/template"
-  "strconv"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
 )
 
-
 type Coordinates struct {
-	Name       string
-	Value      float64
-	Text       string
+	Name  string
+	Value float64
+	Text  string
 }
 
 type PageVariables struct {
-  PageTitle        string
-  PageCoordinates []Coordinates
-  Answer          []float64
+	PageTitle       string
+	PageCoordinates []Coordinates
+	Answer          []float64
 }
 
-
 func main() {
-  http.HandleFunc("/", DisplayCoordinates)
-  http.HandleFunc("/selected", UserSelected)
-  log.Fatal(http.ListenAndServe(getPort(), nil))
+	http.HandleFunc("/", DisplayCoordinates)
+	http.HandleFunc("/selected", UserSelected)
+	log.Fatal(http.ListenAndServe(getPort(), nil))
 }
 
 func getPort() string {
@@ -35,52 +34,51 @@ func getPort() string {
 	return ":8080"
 }
 
+func DisplayCoordinates(w http.ResponseWriter, r *http.Request) {
 
-func DisplayCoordinates(w http.ResponseWriter, r *http.Request){
+	Title := "Solar Energy"
+	MyCoordinates := []Coordinates{
+		Coordinates{"coordinaten", 0, "North"},
+		Coordinates{"coordinates", 0, "South"},
+	}
 
-   Title := "Solar Energy"
-   MyCoordinates := []Coordinates{
-     Coordinates{"coordinaten", 0, "North"},
-     Coordinates{"coordinates", 0, "South"},
-   }
+	MyPageVariables := PageVariables{
+		PageTitle:       Title,
+		PageCoordinates: MyCoordinates,
+	}
 
-  MyPageVariables := PageVariables{
-    PageTitle: Title,
-    PageCoordinates : MyCoordinates,
-    }
+	t, err := template.ParseFiles("select.html") //parse the html file homepage.html
+	if err != nil {                              // if there is an error
+		log.Print("template parsing error: ", err) // log it
+	}
 
-   t, err := template.ParseFiles("select.html") //parse the html file homepage.html
-   if err != nil { // if there is an error
-     log.Print("template parsing error: ", err) // log it
-   }
-
-   err = t.Execute(w, MyPageVariables) //execute the template and pass it the HomePageVars struct to fill in the gaps
-   if err != nil { // if there is an error
-     log.Print("template executing error: ", err) //log it
-   }
+	err = t.Execute(w, MyPageVariables) //execute the template and pass it the HomePageVars struct to fill in the gaps
+	if err != nil {                     // if there is an error
+		log.Print("template executing error: ", err) //log it
+	}
 
 }
 
-func UserSelected(w http.ResponseWriter, r *http.Request){
-  r.ParseForm()
+func UserSelected(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
 
-  northcoord,_ := strconv.ParseFloat(r.Form.Get("coordinaten"), 64)
-  southcoord,_ := strconv.ParseFloat(r.Form.Get("coordinates"), 64)
+	northcoord, _ := strconv.ParseFloat(r.Form.Get("coordinaten"), 64)
+	southcoord, _ := strconv.ParseFloat(r.Form.Get("coordinates"), 64)
 
-  Title := "Your coordinates"
-  MyPageVariables := PageVariables{
-    PageTitle: Title,
-    Answer: []float64{northcoord, southcoord},
-    }
+	Title := "Your coordinates"
+	MyPageVariables := PageVariables{
+		PageTitle: Title,
+		Answer:    []float64{northcoord, southcoord},
+	}
 
- // generate page by passing page variables into template
-    t, err := template.ParseFiles("select.html") //parse the html file homepage.html
-    if err != nil { // if there is an error
-      log.Print("template parsing error: ", err) // log it
-    }
+	// generate page by passing page variables into template
+	t, err := template.ParseFiles("select.html") //parse the html file homepage.html
+	if err != nil {                              // if there is an error
+		log.Print("template parsing error: ", err) // log it
+	}
 
-    err = t.Execute(w, MyPageVariables) //execute the template and pass it the HomePageVars struct to fill in the gaps
-    if err != nil { // if there is an error
-      log.Print("template executing error: ", err) //log it
-    }
+	err = t.Execute(w, MyPageVariables) //execute the template and pass it the HomePageVars struct to fill in the gaps
+	if err != nil {                     // if there is an error
+		log.Print("template executing error: ", err) //log it
+	}
 }
